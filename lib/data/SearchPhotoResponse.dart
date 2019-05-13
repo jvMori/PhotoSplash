ce1175cf23d8e2f96d2351858e14423bf29900cd
Mori <iwona.moryc92@gmail.com>
2019-05-13 17:36:41 +0200

    Update SearchPhotoResponse.dart

--- lib/data/SearchPhotoResponse.dart
+++ lib/data/SearchPhotoResponse.dart
@@ -1,3 +1,15 @@
-class SearchPhotoResponse{
+import 'Photo.dart';
 
+class SearchPhotoResponse{
+  num totalPages;
+  List<Photo> results;
+
+  SearchPhotoResponse({this.totalPages, this.results});
+
+  factory SearchPhotoResponse.fromJson(Map<String, dynamic> json){
+    return SearchPhotoResponse(
+      totalPages: json["total_pages"],
+      results: json["results"]
+    );
+  }
 }
