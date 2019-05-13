300ea16c32a37999bd68500fda927e69a63f3d52
Mori <iwona.moryc92@gmail.com>
2019-05-13 17:32:11 +0200

    Update Photo.dart

--- lib/data/Photo.dart
+++ lib/data/Photo.dart
@@ -2,11 +2,17 @@ class Photo {
   num likes;
   String description;
   User user;
-  String photoUrl;
   Urls urls;
 
-  Photo(this.likes, this.description, this.user, this.photoUrl, this.urls);
+  Photo({this.likes, this.description, this.user, this.urls});
 
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+        likes: json["likes"],
+        description: json["description"],
+        user: json["user"],
+        urls: json["urls"]);
+  }
 }
 
 class User {
@@ -15,13 +21,46 @@ class User {
   String name;
   String portfolioUrl;
   ProfileImage profileImage;
+
+  User(
+      {this.id,
+      this.username,
+      this.name,
+      this.portfolioUrl,
+      this.profileImage});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        id: json["id"],
+        username: json["username"],
+        name: json["name"],
+        portfolioUrl: json["portfolio_url"],
+        profileImage: json["profile_image"]);
+  }
 }
 
-class ProfileImage{
+class ProfileImage {
   String medium;
+
+  ProfileImage({this.medium});
+
+  factory ProfileImage.fromJson(Map<String, dynamic> json) {
+    return ProfileImage(
+      medium: json["medium"],
+    );
+  }
 }
 
-class Urls{
+class Urls {
   String small;
   String full;
+
+  Urls({this.small, this.full});
+
+  factory Urls.fromJson(Map<String, dynamic> json) {
+    return Urls(
+      small: json["small"],
+      full: json["full"],
+    );
+  }
 }
