aaa3fc2c199a0643393aebfee81188c5398aa0d0
jvMori <iwona.moryc92@gmail.com>
2019-05-15 20:57:21 +0200

    Update PhotoItem.dart

--- lib/ui/PhotoItem.dart
+++ lib/ui/PhotoItem.dart
@@ -12,53 +12,55 @@ class PhotoItem extends StatelessWidget {
     return new Card(
       child: new Container(
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: Colors.black12),
+              borderRadius: BorderRadius.circular(5), color: Colors.black12),
           height: 350,
-          padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
+          margin: EdgeInsets.only(left: 0.0, right: 0.0),
+          padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
           child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Expanded(
-                    flex: 3,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        CircleAvatar(
-                           backgroundImage: NetworkImage(
-                               _data.user.profileImage.medium
-                           ),
-                           radius: 18,
-                         ),
-                        Container(
-                          margin: EdgeInsets.only(left: 8.0),
-                          child: Text(_data.user.name,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                                letterSpacing: 1
-                            ),
+              Container(
+                padding: EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 3,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(_data.user.profileImage.medium),
+                            radius: 18,
                           ),
-                        )
-                      ],
+                          Container(
+                            margin: EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              _data.user.name,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  letterSpacing: 1),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  Icon(
-                    Icons.more_horiz,
-                    color: Colors.black45,
-                    size: 40,
-                  ),
-                ],
+                    Icon(
+                      Icons.more_horiz,
+                      color: Colors.black45,
+                      size: 40,
+                    ),
+                  ],
+                ),
               ),
               Expanded(
                 child: FadeInImage.memoryNetwork(
                   placeholder: kTransparentImage,
                   image: _data.urls.small,
-                  height: 250,
-                  fit: BoxFit.fitWidth,
+                  height: 200,
+                  fit: BoxFit.cover,
                 ),
               )
             ],
