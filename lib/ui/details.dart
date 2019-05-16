9e03fb3f22ac074e7bf17df74f488059e8713f25
jvMori <iwona.moryc92@gmail.com>
2019-05-16 22:45:17 +0200

    Update details.dart

--- lib/ui/details.dart
+++ lib/ui/details.dart
@@ -25,22 +25,58 @@ class Details extends StatelessWidget {
             ),
             Container(
               decoration: BoxDecoration(
-                color: Colors.white30
+                color: Color(0xFFe6e8ed)
               ),
               padding: EdgeInsets.all(16.0),
               child: Row(
                 children: <Widget>[
-                  CircularAvatar(_data, 20.0, 0.0)
+                  CircularAvatar(_data, 15.0, 0.0),
+                  Padding(
+                    padding: EdgeInsets.only(left: 16.0),
+                    child: Text("By "+ _data.user.name, style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold
+                    )),
+                  ),
                 ],
               ),
-            )
+            ),
+            DetailsItem(_data.user.name, Icons.calendar_today),
+            DetailsItem(_data.likes.toString() + " Likes", Icons.headset),
+            DetailsItem(_data.likes.toString() + " Downloads", Icons.file_download),
+            DetailsItem(_data.user.name, Icons.color_lens)
           ],
         )
         ),
       );
   }
 }
+class DetailsItem extends StatelessWidget{
+  final String _text;
+  final IconData _icon;
 
+  DetailsItem(this._text, this._icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: <Widget>[
+          Icon(_icon, size: 25,),
+          Padding(
+            padding: const EdgeInsets.only(left:16.0),
+            child: Text(_text, style: TextStyle(
+                color: Colors.black87,
+                fontSize: 15
+            ),),
+          )
+        ],
+      ),
+    );
+  }
+
+}
 //Column(
 //children: <Widget>[
 //Expanded(
