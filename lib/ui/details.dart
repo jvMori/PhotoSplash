671928e487f04e0ad415b1efa14b37595f5cef07
jvMori <iwona.moryc92@gmail.com>
2019-05-16 22:52:08 +0200

    Update details.dart

--- lib/ui/details.dart
+++ lib/ui/details.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/data/Photo.dart';
 import 'package:flutter_app/ui/CircularAvatar.dart';
-import 'package:flutter_app/ui/picture.dart';
 
 class Details extends StatelessWidget {
   final Photo _data;
@@ -75,26 +74,5 @@ class DetailsItem extends StatelessWidget{
       ),
     );
   }
-
 }
-//Column(
-//children: <Widget>[
-//Expanded(
-//child: Container(
-//height: 300,
-//child: Picture(_data, 0.0, 0.0),
-//)
-//),
-//Container(
-//padding: EdgeInsets.all(8.0),
-//decoration: BoxDecoration(
-//color: Colors.white30,
-//),
-//child: Row(
-//children: <Widget>[
-//CircularAvatar(_data, 15.0, 0.0)
-//],
-//),
-//)
-//],
-//);
+
